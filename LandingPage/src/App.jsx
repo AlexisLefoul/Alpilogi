90b3ec15e299d1f458aa47a08174dd93d45d2c6a
alexis lefoul <alex.lefoul@orange.fr>
2022-12-06 23:35:18 +0100

    init-2

--- LandingPage/src/App.jsx
+++ LandingPage/src/App.jsx
@@ -1,14 +1,75 @@
-import LOGO from "../src/assets/Logo v0.png";
-
+import React, { useEffect, useState } from "react";
+import Timer from "./components/Timer";
+import { Logo } from "./components/Logo";
 
 function App() {
+  let deadline = "December, 31, 2022";
+  const [isShowingAlert, setShowingAlert] = React.useState(false);
+  const [data, setData] = useState();
+  const [email, setEmail] = useState("");
+  /*const submit = async () => {
+    setShowingAlert(true);
+    await axios
+      .post("https://api.ostava.fibeer.fr/newsletter", { email })
+      .then((response) => {
+        setData(response) && setShowingAlert(true);
+      })
+      .catch((error) => {
+        setData(error.response) && setShowingAlert(true);
+      });
+  };
+
+  useEffect(() => {
+    if (isShowingAlert === true) {
+      setTimeout(() => {
+        setShowingAlert(false);
+      }, 6000);
+    }
+  }, [isShowingAlert]);*/
+
   return (
-    <div className="header">
-      <a className="logo-position">
-        <img src={LOGO} className="logo-taille" />
-      </a>
-    </div>
+    <>
+      <div className="container">
+        <div className="left">
+          <Logo className="logo" />
+        </div>
+        <div className="right">
+          <h1>Dome Of Help !</h1>
+          <p className="slogan">
+            L'application qui révolutionnera bientôt ton <br /> garde-manger !
+          </p>
+          <Timer deadline={deadline} />
+          <p className="notif" style={{ marginLeft: 50, marginTop: 50 }}>
+            Soit notifié de son arrivée !
+          </p>
+          <div className="container-input">
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              placeholder="Entrez votre adresse mail"
+              type="mail"
+            />
+            <button className="btn-input">
+              S'inscrire
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+/*      {data?.status === 400 && isShowingAlert && (
+        <>
+          <div
+            className={`toast ${
+              isShowingAlert ? "alert-shown" : "alert-hidden"
+            }`}
+            onTransitionEnd={() => setShowingAlert(false)}
+          >
+            <p>{data?.data?.message}</p>
+          </div>
+        </>
+      )}*/
---
+++ LandingPage/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../assets/Logov0.png";
+export const Logo = (props) => {
+  return (
+    <>
+      <img {...props} src={logo} alt="" />
+    </>
+  );
+};
