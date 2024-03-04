a04614e0d63e3156776503bae55ab8386cb2356f
AlexisLefoul <alex.lefoul@orange.fr>
2024-03-04 21:45:56 +0100

    Update package and datas

--- App/Pro/components/BoxAction.js
+++ App/Pro/components/BoxAction.js
@@ -3,19 +3,27 @@ import { StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 import { VStack, HStack } from "@gluestack-ui/themed";
 import { useNavigation } from "@react-navigation/native";
 import { FontSize, FontFamily, Color } from "../GlobalStyles";
+import { useUser } from "../UserContext";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import IconOcticons from "react-native-vector-icons/Octicons";
 import LineSep from "../assets/line.png";
 
 export default function BoxAction({ nav, text, icon, last }) {
   const navigation = useNavigation();
+  const { logout } = useUser();
+
+  const handleOnpressOut = () => {
+    navigation.navigate(nav);
+    logout();
+  };
+
+  const handleOnpress = () => {
+    navigation.navigate(nav);
+  };
+
   return (
     <VStack paddingBottom={5}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate(nav);
-        }}
-      >
+      <TouchableOpacity onPress={last ? handleOnpressOut : handleOnpress}>
         <HStack style={styles.containerAction}>
           <HStack space="lg" alignItems="center">
             <MaterialCommunityIcons name={icon} size={24} color={Color.bleu1} />
@@ -32,7 +40,6 @@ export default function BoxAction({ nav, text, icon, last }) {
     </VStack>
   );
 }
-
 const styles = StyleSheet.create({
   containerAction: {
     padding: 18,
